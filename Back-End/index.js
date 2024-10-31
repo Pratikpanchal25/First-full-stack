18aac804195056a435f413a0c7c51cb893e28252
Pratik Panchal <pratikpanchal20040502@gmail.com>
2024-11-01 00:01:14 +0530

    summay

--- Back-End/index.js
+++ Back-End/index.js
@@ -4,7 +4,8 @@ import cors from "cors";
 import router from "./src/user.route.js";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import dotenv from "dotenv"
+dotenv.config()
 const app = express();
 
 const connectDB = async () => {
@@ -16,14 +17,14 @@ const connectDB = async () => {
     throw error;
   }
 };
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-connectDB().then(() => console.log("MONGODB Connected"));
+// connectDB().then(() => console.log("MONGODB Connected"));
 
-app.listen(8000, () => console.log("The App Is Listening On 5173"));
+app.listen(process.env.PORT, () => console.log(`The App Is Listening On ${process.env.PORT}`));
 
 app.use("/", router);
 
